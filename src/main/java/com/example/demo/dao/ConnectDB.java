a01eddced8777889520b7a5fdb32b7c8eb96a24a
Khoa <tsukihuman@Gmail.com>
2021-11-19 21:31:14 +0700

    Ingredient
    Nhập hàng

--- src/main/java/com/example/demo/dao/ConnectDB.java
+++ src/main/java/com/example/demo/dao/ConnectDB.java
@@ -3,5 +3,5 @@ package com.example.demo.dao;
 public class ConnectDB {
     public static final String url = "jdbc:mariadb://localhost:3307/dulieuquanlifb";
     public static final String user = "root";
-    public static final String pw = "123456";
+    public static final String pw = "1jwdjpajtgjm";
 }
